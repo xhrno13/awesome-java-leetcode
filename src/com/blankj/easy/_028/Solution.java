8a915dede961597c8057c0806b7b37aa3d470f91
tyreke.xu <tyreke.xu@qunar.com>
2020-04-03 10:28:26 +0800

    add

--- src/com/blankj/easy/_028/Solution.java
+++ src/com/blankj/easy/_028/Solution.java
@@ -11,12 +11,21 @@ package com.blankj.easy._028;
 public class Solution {
     public int strStr(String haystack, String needle) {
         int l1 = haystack.length(), l2 = needle.length();
-        if (l1 < l2) return -1;
+        if (l2 == 0) {
+            return 0;
+        }
+        if (l1 < l2) {
+            return -1;
+        }
         for (int i = 0; ; i++) {
-            if (i + l2 > l1) return -1;
+            if (i + l2 > l1) {
+                return -1;
+            }
             for (int j = 0; ; j++) {
                 if (j == l2) return i;
-                if (haystack.charAt(i + j) != needle.charAt(j)) break;
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
             }
         }
     }
