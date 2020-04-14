c4c92ac5f65a0704a38376fffefabc71305afc28
tyreke.xu <tyreke.xu@qunar.com>
2020-04-14 09:58:48 +0800

    add

--- src/com/blankj/easy/_100/Solution.java
+++ src/com/blankj/easy/_100/Solution.java
@@ -13,8 +13,12 @@ import com.blankj.structure.TreeNode;
  */
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null || q == null) return p == q;
-        if (p.val != q.val) return false;
+        if (p == null || q == null) {
+            return true;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 
--- src/com/blankj/easy/_101/Solution.java
+++ src/com/blankj/easy/_101/Solution.java
@@ -3,8 +3,6 @@ package com.blankj.easy._101;
 
 import com.blankj.structure.TreeNode;
 
-import java.util.LinkedList;
-
 /**
  * <pre>
  *     author: Blankj
@@ -14,35 +12,42 @@ import java.util.LinkedList;
  * </pre>
  */
 public class Solution {
-//    public boolean isSymmetric(TreeNode root) {
-//        return root == null || helper(root.left, root.right);
-//    }
-//
-//    private boolean helper(TreeNode left, TreeNode right) {
-//        if (left == null || right == null) return left == right;
-//        if (left.val != right.val) return false;
-//        return helper(left.left, right.right) && helper(left.right, right.left);
-//    }
-
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) return true;
-        LinkedList<TreeNode> q = new LinkedList<>();
-        q.add(root.left);
-        q.add(root.right);
-        TreeNode left, right;
-        while (q.size() > 1) {
-            left = q.pop();
-            right = q.pop();
-            if (left == null && right == null) continue;
-            if (left == null || right == null) return false;
-            if (left.val != right.val) return false;
-            q.add(left.left);
-            q.add(right.right);
-            q.add(left.right);
-            q.add(right.left);
-        }
-        return true;
+        return isMirror(root, root);
     }
+
+    public boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+        return (t1.val == t2.val)
+                && isMirror(t1.right, t2.left)
+                && isMirror(t1.left, t2.right);
+    }
+
+
+//    public boolean isSymmetric(TreeNode root) {
+//        if (root == null) return true;
+//        LinkedList<TreeNode> q = new LinkedList<>();
+//        q.add(root.left);
+//        q.add(root.right);
+//        TreeNode left, right;
+//        while (q.size() > 1) {
+//            left = q.pop();
+//            right = q.pop();
+//            if (left == null && right == null) continue;
+//            if (left == null || right == null) return false;
+//            if (left.val != right.val) return false;
+//            q.add(left.left);
+//            q.add(right.right);
+//            q.add(left.right);
+//            q.add(right.left);
+//        }
+//        return true;
+//    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
