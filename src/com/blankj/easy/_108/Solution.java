22eeedb369885e6b89d39715bff8c99a3be7fe0a
tyreke.xu <tyreke.xu@qunar.com>
2020-04-16 09:47:33 +0800

    add

--- src/com/blankj/easy/_108/Solution.java
+++ src/com/blankj/easy/_108/Solution.java
@@ -12,13 +12,17 @@ import com.blankj.structure.TreeNode;
  * </pre>
  */
 public class Solution {
-    public TreeNode sortedArrayToBST(int[] nums) {
-        if (nums == null || nums.length == 0) return null;
+    private TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
         return helper(nums, 0, nums.length - 1);
     }
 
     private TreeNode helper(int[] nums, int left, int right) {
-        if (left > right) return null;
+        if (left > right) {
+            return null;
+        }
         int mid = (left + right) >>> 1;
         TreeNode node = new TreeNode(nums[mid]);
         node.left = helper(nums, left, mid - 1);
